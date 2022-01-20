579ea49153043df137f02abbe0cab6133029183f
Bruno_odinukweze <brunofreelancing@gmail.com>
2022-01-20 12:32:31 +0100

    added Procfile

--- receipts_generator/settings.py
+++ receipts_generator/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['dukka-test.herokuapp.com']
+ALLOWED_HOSTS = ['dukka-test.herokuapp.com', 'localhost']
 
 
 # Application definition
